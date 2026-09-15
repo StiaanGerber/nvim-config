6309bd587256b73753900579bc34ba26c0ec3906
Stiaan Gerber <stiaan.gerber@ai.matogen.com>
2026-09-15 10:51:22 +0200

    Added diffview.nvim

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,12 @@
+return {
+  "sindrets/diffview.nvim",
+  cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+  keys = {
+    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+    { "<leader>gD", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
+  },
+  opts = {
+    -- You can add your custom configuration here if needed
+  },
+}
+
